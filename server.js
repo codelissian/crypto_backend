d75ecd6e2a531a6a2751f7c961a7102810c75127
Ankush <Personal@Ankushs-MacBook-Pro.local>
2023-11-11 09:45:00 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,73 @@
+import express from 'express';
+import nodemailer from 'nodemailer';
+import multer from 'multer';
+import { extname } from 'path';
+import dotenv from 'dotenv';
+const app = express();
+const port = 3000;
+
+
+dotenv.config();
+// Set up Nodemailer
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+// Set up Multer for file uploads
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Save images in the 'uploads' directory
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.use(express.json());
+
+// Serve static files (images) from the 'uploads' directory
+app.use('/uploads', express.static('uploads'));
+
+app.post('/uploadImageAndSendEmail', upload.single('image'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No image provided' });
+  }
+
+  const imagePath = req.file.path;
+  const originalName = req.file.originalname;
+
+  const { to, subject, text } = req.body; // Receive recipient, subject, and text from the request body
+
+  const mailOptions = {
+    from: 'your_email@gmail.com',
+    to,
+    subject,
+    text,
+    attachments: [
+      {
+        filename: originalName,
+        path: imagePath,
+      },
+    ],
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).json({ error: 'Email not sent' });
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.json({ message: 'Email sent successfully' });
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
